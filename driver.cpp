012434b8898ede06dda105e63fb1446bca743b61
afhudson42 <afhudson42@tntech.edu>
2024-09-18 15:34:04 -0500

    Print Menu

--- driver.cpp
+++ driver.cpp
@@ -4,5 +4,14 @@ using namespace std;
 
 int main()
 {
+    int userInput;
+
+    cout << "1.) Would you like to add a customer? " << endl;
+    cout << "2.) Print parking lot status. " << endl;
+    cout << "3.) Would you like to remove a customer? " << endl;
+    cout << "4.) View all customer in the paring lot. " << endl;
+    cout << "5.) Would you like to end the program? " << endl;
+
     
+    return 0;
 }
