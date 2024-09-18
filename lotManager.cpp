169179d90cf263174c75abf1472aeb6a228e200e
GWSonger <gwsonger42@tntech.edu>
2024-09-18 15:20:20 -0500

    Comm1

--- lotManager.cpp
+++ lotManager.cpp
@@ -1,1 +1,21 @@
-#include "connection.h"
+#include "connection.h"#include "lotManager.h"
+
+parkingLot* parkLotArr = new parkingLot[size];
+
+parkingLot::lotConstruct()
+{}
+
+parkingLot lotDeconstruct ()
+{}
+
+parkingLot makeLot ()
+{}
+
+parkingLot removeLot ()
+{}
+
+parkingLot getLot ()
+{}
+
+parkingLot seeLot ()
+{}
