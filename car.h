ead79eb40a2595b5af767d8d4a90a8d6b2f7230c
JoyhlGwinn <jgwinn42@tntech.edu>
2024-09-18 14:48:02 -0500

    Update car.h

--- car.h
+++ car.h
@@ -1,4 +1,21 @@
 #ifndef CAR_H
 #define CAR_H
 
+#include <iostream>
+using namespace std;
+
+class car
+{
+    private:
+    string licencsePlate;
+    string Make;
+    string Model;
+    string OwnerName;
+    int enumeration;
+    int year;
+
+    public:
+    car(string, string, string, string, int, int);
+};
+
 #endif
