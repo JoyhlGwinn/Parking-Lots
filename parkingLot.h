5417e30ef1859296a20a92f582a8c72e872ab834
JDHart42 <jdhart42@tntech.edu>
2024-09-25 11:50:58 -0500

    parkingLot.h

--- parkingLot.h
+++ parkingLot.h
@@ -12,6 +12,17 @@ class ParkingLot {
     int** parkingTimes;
     int** car;
 
+public:
+parkingLot(sring name, int time);
+~parkingLot();
+
+bool parkCar(int** car, int time);
+void printLotStatus() const;
+void setLocation(const string& location);
+void removeCar(int** car);
+
+private:
+string lotLocation;
 
 };
 #endif
