04c3c5a2cd867b7fd3ed228e55d7a610825a03fb
JDHart42 <jdhart42@tntech.edu>
2024-09-25 15:13:10 -0500

    parkingLot.h

--- parkingLot.h
+++ parkingLot.h
@@ -1,28 +1,23 @@
 #ifndef PARKINGLOT_H
 #define PARKINGLOT_H
 #include "car.h"
-#include <string>
 using namespace std;
 
 class ParkingLot {
     private:
-    string lotName;
     int lotCapacity;
     int currentOccupancy;
-    int** parkingTimes;
-    int** car;
+    int* parkingTimes;
+    Car** parkedCars;
 
 public:
-ParkingLot(sring name, int time);
+ParkingLot(int capacaity);
 ~ParkingLot();
 
-bool parkCar(int** car, int time);
-void printLotStatus() const;
-void setLocation(const string& location);
-void removeCar(int** car);
-
-private:
-string lotLocation;
+bool isAvailable(int spot);
+void assignCar(int spot, Car* car, int rentalTime);
+void releaseSpot(int spot);
+void displayStatus() const;
 
 };
 #endif
