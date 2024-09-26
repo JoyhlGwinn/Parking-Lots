5352bb9199e3d1794ad4bd5f8f2e84f2e4f35b45
JDHart42 <jdhart42@tntech.edu>
2024-09-26 17:01:53 -0500

    parkingLot.cpp

--- parkingLot.cpp
+++ parkingLot.cpp
@@ -1,5 +1,26 @@
-#include "parkingLot.h"
+#include "ParkingLot.h"
 #include <iostream>
-using namespace std;
+using namespace std; 
 
-ParkingLot(string name, int capacity)
+ParkingLot::ParkingLot(int capacity)
+    : lotCapacity(capacity), currentOccupancy(0) {
+    // Initialize arrays
+    parkingTimes = new int[lotCapacity]();
+    parkedCars = new Car*[lotCapacity];
+    for (int i = 0; i < lotCapacity; ++i) {
+        parkedCars[i] = nullptr;
+    }
+}
+
+ParkingLot::~ParkingLot() {
+    delete[] parkingTimes;
+    delete[] parkedCars;
+}
+
+bool ParkingLot::isAvailable(int spot) const {
+    if (spot < 0 || spot >= lotCapacity) {
+        cerr << "Invalid spot number.\n";
+        return false;
+    }
+    return parkedCars[spot] == nullptr;
+}
