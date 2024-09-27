5a283a8a3a6136881d1a7b3a305a93887eb2db08
JDHart42 <jdhart42@tntech.edu>
2024-09-26 20:05:35 -0500

    ParkingLot.cpp / ,h

--- parkingLot.cpp
+++ parkingLot.cpp
@@ -24,3 +24,45 @@ bool ParkingLot::isAvailable(int spot) const {
     }
     return parkedCars[spot] == nullptr;
 }
+
+void ParkingLot::assignCar(int spot, Car* car, int rentalTime) {
+    if (spot < 0 || spot >= lotCapacity) {
+        cerr << "Invalid spot number.\n";
+        return;
+    }
+    if (parkedCars[spot] != nullptr) {
+        cerr << "Spot is already occupied.\n";
+        return;
+    }
+    parkedCars[spot] = car;
+    parkingTimes[spot] = rentalTime;
+    ++currentOccupancy;
+}
+
+void ParkingLot::releaseSpot(int spot) {
+    if (spot < 0 || spot >= lotCapacity) {
+        cerr << "Invalid spot number.\n";
+        return;
+    }
+    if (parkedCars[spot] == nullptr) {
+        cerr << "Spot is already empty.\n";
+        return;
+    }
+    parkedCars[spot] = nullptr;
+    parkingTimes[spot] = 0;
+    --currentOccupancy;
+}
+
+void ParkingLot::displayStatus() const {
+    cout << "Parking Lot Status:\n";
+    cout << "Capacity: " << lotCapacity << "\n";
+    cout << "Current Occupancy: " << currentOccupancy << "\n";
+    for (int i = 0; i < lotCapacity; ++i) {
+        cout << "Spot " << i << ": ";
+        if (parkedCars[i] != nullptr) {
+            cout << "Occupied (rental time left: " << parkingTimes[i] << ")\n";
+        } else {
+            cout << "Available\n";
+        }
+    }
+}
