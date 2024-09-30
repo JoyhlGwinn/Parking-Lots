1eb5332034cbed07248018749eb82e236e7935ad
Connor Jordan <56240039+ComputerConnor@users.noreply.github.com>
2024-09-30 11:15:24 -0500

    Update car.cpp

--- car.cpp
+++ car.cpp
@@ -1,4 +1,35 @@
 #include "car.h"
 
-car::car()
+Car::Car(string make, string model, string licensePlate, string color) //Read 02-CSC1310 OOP+introC++ on iLearn
+: make(make), model(model), licensePlate(licensePlate), color(color) {} //Initialization by calling constructor for class and
+                                                                        //create objects make, model, licenseplate, and color
+string Car::getDetails() const{
+    return "\nMake: " + make + "\nModel: " + model + "\nLicense Plate: " + licensePlate + "\nColor: " + color + "\n";
+}
+void Car::updateLicensePlate(string newPlate) {
+    licensePlate = newPlate;
+}
 
+void Car::updateColor(string newColor) {
+    color = newColor;
+}
+
+string Car::getMake() const {
+    return make;
+}
+
+string Car::getModel() const {
+    return model;
+}
+
+string Car::getLicensePlate() const {
+    return licensePlate;
+}
+
+string Car::getColor() const {
+    return color;
+}
+
+
+
+//Usage in Driver.cpp: Car myCar("Lamborghini", "Huracan", "ABC123", "Yellow");
